0cd2db98480b19626e89eff97f744b4aa66db9d5
neominalsolutions <neominal.solutions@gmail.com>
2023-10-13 13:26:53 +0300

    spring jwt

--- src/main/java/com/akbank/springjwt/SpringjwtApplication.java
+++ src/main/java/com/akbank/springjwt/SpringjwtApplication.java
@@ -2,9 +2,6 @@ package com.akbank.springjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.akbank.springjwt.config.JwtSettings;
 
 @SpringBootApplication
 public class SpringjwtApplication {
